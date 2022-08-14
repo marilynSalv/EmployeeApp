d671a80f7b65c4c5b723b10ab56fc9603b703fde
marilyn <msalvatierra1356@gmail.com>
2022-08-14 17:07:24 -0500

    update migrations

--- EmployeeApp.Dal/Migrations/20220810210317_EmployeeDb.Designer.cs
+++ EmployeeApp.Dal/Migrations/20220810210317_EmployeeDb.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeApp.Dal.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeApp.Dal.Migrations
 {
     [DbContext(typeof(PlayGroundContext))]
-    partial class PlayGroundContextModelSnapshot : ModelSnapshot
+    [Migration("20220810210317_EmployeeDb")]
+    partial class EmployeeDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
