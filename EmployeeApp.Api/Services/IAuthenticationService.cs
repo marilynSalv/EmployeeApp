0d6506dadc4ac77340bbc8301d23b851a32a4070
marilyn <msalvatierra1356@gmail.com>
2022-08-30 01:50:32 -0500

    cleanup controller

--- EmployeeApp.Api/Services/IAuthenticationService.cs
+++ EmployeeApp.Api/Services/IAuthenticationService.cs
@@ -1,11 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using EmployeeApp.Dal.Dtos;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace EmployeeApp.Api.Services
 {
     public interface IAuthenticationService
     {
+        Task<IdentityResult> CreateUser(RegisterDto registerDto);
+        Task<AuthResponseDto> LoginUser(LoginDto loginDto);
         Task AddRefreshToken(string username, string refreshToken);
         Task<bool> IsRefreshTokenValid(string username, string refreshToken);
         Task InvalidateRefreshToken(string username);
+        Task<RefreshTokenDto> RefreshToken(RefreshTokenDto dto);
     }
 }
--- EmployeeApp.Api/Services/IRefreshTokenGenerator.cs
+++ EmployeeApp.Api/Services/IRefreshTokenGenerator.cs
@@ -1,7 +1,12 @@
-﻿namespace EmployeeApp.Api.Services
+﻿using EmployeeApp.Dal.Dtos;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Api.Services
 {
     public interface IRefreshTokenGenerator
     {
-        string GenerateToken();
+        Task<RefreshTokenDto> CreateTokenAndRefresh(string username, Claim[] claims);
+        Task AddRefreshToken(string username, string refreshToken);
     }
 }
