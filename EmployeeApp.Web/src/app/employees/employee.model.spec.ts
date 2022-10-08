c3f4128fa744724effb885def1c481b2ada918a1
marilyn <msalvatierra1356@gmail.com>
2022-10-08 01:19:58 -0500

    update model spec

--- EmployeeApp.Web/src/app/employees/employee.model.spec.ts
+++ EmployeeApp.Web/src/app/employees/employee.model.spec.ts
@@ -3,9 +3,15 @@ import { EmployeeManagementDto } from './employee.model';
 describe('Employee', () => {
   it('should create an instance', () => {
     const emp: EmployeeManagementDto = {
+      id: 1,
       firstName: "Test",
       lastName: "test",
       email: "test",
+      companyId: null,
+      managerId: 1,
+      isManager: false,
+      zipCode: "33613",
+
     };
     expect(emp).toBeTruthy();
   });
