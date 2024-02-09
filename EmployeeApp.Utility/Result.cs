3ad9d331c22ee03b6112ef7ad838be8cd1b5f3af
marilyn <msalvatierra1356@gmail.com>
2024-02-09 15:17:34 -0600

    wip

---
+++ EmployeeApp.Utility/Result.cs
@@ -0,0 +1,55 @@
+﻿namespace EmployeeApp.Utility;
+
+public class Result
+{
+    public bool Success { get; set; }
+    public string Error { get; }
+    public bool IsFailure => !Success;
+
+    protected Result(bool success, string error)
+    {
+        if(success && error != string.Empty)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if(!success && error == string.Empty) 
+        {
+            throw new InvalidOperationException();
+        }
+
+        Success = success;
+        Error = error;
+    }
+
+    public static Result Fail(string message)
+    {
+        return new Result(false, message);
+    }
+
+    public static Result<T> Fail<T>(string message)
+    {
+        return new Result<T>(default, false, message);
+    }
+
+    public static Result Ok()
+    {
+        return new Result(true, string.Empty);
+    }
+
+    public static Result<T> Ok<T>(T value)
+    {
+        return new Result<T>(value, true, string.Empty);
+    }
+}
+
+public class Result<T> : Result
+{
+    public T Value { get; set; }
+
+    protected internal Result (T value, bool success, string error)
+        : base(success, error)
+    {
+        Value = value;
+    }
+}
