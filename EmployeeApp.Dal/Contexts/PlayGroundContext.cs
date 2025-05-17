ab41e86112d40d7170fbab3cba110dd58170bac3
marilyn <msalvatierra1356@gmail.com>
2025-05-16 20:01:34 -0500

    comment

--- EmployeeApp.Dal/Contexts/PlayGroundContext.cs
+++ EmployeeApp.Dal/Contexts/PlayGroundContext.cs
@@ -24,7 +24,7 @@ namespace EmployeeApp.Dal.Contexts
             modelBuilder.Entity<ApplicationUser>().ToTable("asp_net_users"); //represents user
             modelBuilder.Entity<IdentityRole<Guid>>().ToTable("asp_net_roles"); //represents role; A role is used to define a user's position or responsibility within a system. example Admin vs user vs manager
             modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("asp_net_user_claims"); //Represents a claim that a user possesses. Represents specific pieces of information about a user. attribute-based access control. example name, email adddress, permission.
-            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("asp_net_user_tokens");//Represents an authentication token for a user.
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("asp_net_user_tokens");//Represents an authentication token for a user usually for external logins like google.
             modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("asp_net_user_logins"); //Associates a user with a login
             modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("asp_net_user_roles"); //A join entity that associates users and roles.
             modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("asp_net_role_claims"); //Represents a claim that's granted to all users within a role.
