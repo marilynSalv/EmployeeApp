c8a677434b2b35902931aab39b00c8492d45255f
marilyn <msalvatierra1356@gmail.com>
2026-08-11 18:08:51 -0500

    fix

--- EmployeeApp.Api/Program.cs
+++ EmployeeApp.Api/Program.cs
@@ -1,70 +1,17 @@
 using EmployeeApp.Api;
-using EmployeeApp.Application.Dtos;
-using EmployeeApp.Dal.Contexts;
-using EmployeeApp.Dal.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
-using System;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
+var connectionString = config.GetConnectionString("PlayGroundContext");
+var jwtSecret = config["ApplicationSettings:JwtSecret"];
 
-// -------------------- SERVICES --------------------
-
-builder.Services.Configure<ApplicationSettings>(
-    config.GetSection("ApplicationSettings"));
-
-builder.Services.AddDbContextPool<PlayGroundContext>(options =>
-{
-    options.UseNpgsql(config.GetConnectionString("PlayGroundContext"));
-    options.UseSnakeCaseNamingConvention();
-});
-
-builder.Services.AddIdentityCore<ApplicationUser>(options =>
-{
-    options.User.RequireUniqueEmail = true;
-    options.Password.RequireDigit = true;
-    options.Password.RequiredLength = 7;
-    options.Password.RequireUppercase = true;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
-    options.Lockout.MaxFailedAccessAttempts = 3;
-})
-.AddRoles<IdentityRole<Guid>>()
-.AddEntityFrameworkStores<PlayGroundContext>()
-.AddDefaultTokenProviders()
-.AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>(TokenOptions.DefaultAuthenticatorProvider);
-
-builder.Services.AddAuthentication(auth =>
-{
-    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = false;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        RequireExpirationTime = true,
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(config["ApplicationSettings:JwtSecret"]))
-    };
-});
+builder.Services.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
---
+++ EmployeeApp.Dal/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using EmployeeApp.Infrastructure.Contexts;
+using EmployeeApp.Infrastructure.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace EmployeeApp.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddData(this IServiceCollection services, string connectionString, string jwtSecret)
+    {
+        services.AddDbContextPool<PlayGroundContext>(options =>
+        { 
+            options.UseNpgsql(connectionString);
+            options.UseSnakeCaseNamingConvention();
+        });
+
+        services.AddIdentityCore<ApplicationUser>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 7;
+            options.Password.RequireUppercase = true;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+            options.Lockout.MaxFailedAccessAttempts = 3;
+        })
+        .AddRoles<IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<PlayGroundContext>()
+        .AddDefaultTokenProviders()
+        .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>(TokenOptions.DefaultAuthenticatorProvider);
+
+        services.AddAuthentication(auth =>
+        {
+            auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                RequireExpirationTime = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSecret))
+            };
+        });
+
+        return services;
+    }
+}
